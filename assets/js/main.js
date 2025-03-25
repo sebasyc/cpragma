9144dba345f984acb722ae1c482e053572d223bd
Sebastián Campos <pseba@outlook.com>
2025-03-25 12:52:17 -0300

    ajustes de animaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@ $(document).ready( function() {
 
     $('.btn-colapse-xs').click(function() {
         $(this).toggleClass('open')
-        $('.nav-menu').slideToggle()
+        $('.nav-menu').slideToggle('fast')
         $('.main-content').toggleClass('blur')
+        $('body').toggleClass('open-menu')
     })
 
     $('.nav-menu li a').click(function() {
         if($(window).width() < 991) {
             $('.btn-colapse-xs').toggleClass('open')
-            $('.nav-menu').slideUp()
+            $('.nav-menu').slideUp('fast')
             $('.main-content').toggleClass('blur')
+            $('body').toggleClass('open-menu')
         }
     })
 
@@ -24,3 +26,47 @@ $(document).ready( function() {
     $('[data-scroll]').on('click', scrollToSection);
 
 })
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".hero-bg", {
+    backgroundPositionY: "90%",
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".hero-bg",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1
+    }
+});
+
+gsap.from(".item-globe1", {
+    y: 200,
+    duration: 1,
+    scrollTrigger: '.item-globe1',
+    stagger: 0.05
+});
+
+gsap.from(".item-globe2", {
+    y: 200,
+    duration: 1,
+    scrollTrigger: '.item-globe2',
+    stagger: 0.05
+});
+
+gsap.from(".logo", {
+    opacity: 0,
+    duration: 1
+});
+
+gsap.from(".menu-item", {
+    opacity: 0,
+    duration: 1
+});
+
+gsap.from(".title-hero", {
+    x: -100,
+    ease: "power3.out",
+    duration: 2,
+    opacity: 0
+});
